e844bef1df78f40c38e275865e7cf80780a6239a
Brandon Maas <5994983+bmaas@users.noreply.github.com>
2018-08-03 13:06:56 -0600

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,6 +1,6 @@
 ﻿var organizationSettings = {
     //The url that points to team city
-    teamCityUrl: 'https://63.157.123.106:3500',
+    teamCityUrl: 'https://63.157.123.106:2222',
 
     //Only show builds for branches that satisfy the predicate
     branchFilter: function(branch) {
